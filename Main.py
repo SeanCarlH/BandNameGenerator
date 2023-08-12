17f5febe2d55601920d081ef2333823e930e544d
SeanCarlH <sharr079@fiu.edu>
2023-08-12 12:55:07 -0400

    Band name generator code

---
+++ Main.py
@@ -0,0 +1,7 @@
+# Band Name Generator
+
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in? ")
+animal = input("What is the name of your favorite animal? ")
+
+print("The name of your band could be " + city + " " + animal)
